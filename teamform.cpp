d45d2fa65eb4c30197b8aaacf915b5a3611a710d
VENU MADHAV YATAM <yvm224@gmail.com>
2017-06-26 15:00:14 +0530

    Create teamform.cpp

---
+++ teamform.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x;
+	cin>>x;
+	while(x--)
+	{
+		int a,b;
+		cin>>a>>b;
+		for(int y=0;y<b;y++)
+		{
+			int u,v;
+			cin>>u>>v;
+		}
+		if(a%2!=0)
+			cout<<"no\n";
+		else
+		cout<<"yes\n";
+	}
+	return 0;
+}  
